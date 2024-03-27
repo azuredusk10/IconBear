453172e478be8092c83bf01b28f8debc28f4eee2
Chris Wood <hello@chris-wood.design>
2024-03-27 01:18:11 +0000

    added bindings for details panel

--- src/DetailsPanel.js
+++ src/DetailsPanel.js
@@ -4,6 +4,22 @@ import Gtk from 'gi://Gtk';
 export const DetailsPanel = GObject.registerClass({
   GTypeName: 'IcoDetailsPanel',
   Template: 'resource:///com/github/azuredusk10/IconManager/ui/DetailsPanel.ui',
+  Properties: {
+    Filepath: GObject.ParamSpec.string(
+      'filepath',
+      'Filepath',
+      'The filepath to the icon image to be displayed',
+      GObject.ParamFlags.READWRITE,
+      ''
+    ),
+    Label: GObject.ParamSpec.string(
+      'label',
+      'Label',
+      'The icon label',
+      GObject.ParamFlags.READWRITE,
+      ''
+    )
+  }
 }, class extends Gtk.Widget {
   constructor(params){
     super(params);

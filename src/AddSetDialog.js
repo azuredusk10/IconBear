0c46c854e9b46b01dd8b5775a8e0d257dd46bb27
Chris Wood <hello@chris-wood.design>
2024-04-12 03:23:53 +0100

    Take an educated guess at whether an icon being imported is a color icon, duotone icon, filled icon or outlined icon using regular expression matches on the svg file contents.

--- src/AddSetDialog.js
+++ src/AddSetDialog.js
@@ -75,11 +75,47 @@ export const AddSetDialog = GObject.registerClass({
         const width = pixbuf.width;
         const height = pixbuf.height;
 
-        // Parse the SVG file
+        // Load the contents of the SVG file
         const gFile = Gio.File.new_for_path(iconPath);
         const [, contents] = gFile.load_contents(null);
+        const stringContents = new TextDecoder().decode(contents);
+        //console.log(stringContents);
+        let style; // 1 = outlined, 2 = filled, 3 = duotone, 4 = color
 
+        // Take an educated guess as to the icon's style
 
+        // Detect if a "stroke" attribute is present, but not if it's followed by "none"
+        const strokeMatches = stringContents.match(/\bstroke=(?!"none")/g) || [];
+        // console.log('stroke matches', strokeMatches.length);
+
+        // Detect if a "fill" attribute is present, but not if it's followed by "none"
+        const fillMatches = stringContents.match(/\bfill=(?!"none")/g) || [];
+        // console.log('fill matches', fillMatches.length);
+
+        // Detect the number of colours present
+        const uniqueColors = this.countUniqueColorsFromString(stringContents);
+        // console.log('colors', uniqueColors)
+
+        if(uniqueColors > 2){
+          // It has multiple colors, it's a color icon
+          style = 4;
+
+        } else if(uniqueColors == 2){
+          // It has 2 colors, it's a duotone icon
+          style = 3;
+
+        } else if(fillMatches.length > 0){
+          // It has at least 1 fill, it's a filled icon
+          style = 2;
+
+        } else if(strokeMatches.length > 0){
+          // It contains no fills, only strokes; it's an outlined icon
+          style = 1;
+
+        }
+
+        console.log('style', style);
+        console.log('name', info.get_name());
 
 
         const iconMeta = {
@@ -96,6 +132,26 @@ export const AddSetDialog = GObject.registerClass({
     this._add_set_dialog.title = `Import ${iconsCount} icons`;
 
 
+  }
+
+  countUniqueColorsFromString(str) {
+    // Regular expression to match hex color codes
+    // Source: https://stackoverflow.com/a/53330328
+    const hexColorRegex = /#(?:(?:[\da-f]{3}){1,2}|(?:[\da-f]{4}){1,2})/gi;
+
+    // Find all matches of hex color codes in the string
+    const matches = str.match(hexColorRegex) || [];
+
+    // Create a Set to store unique colors
+    const uniqueColors = new Set(matches.map(match => match.replace('#', '')));
+
+    // Check if "currentColor" is present in the string and count it as an additional color
+    if (/\bcurrentColor\b/i.test(str)) {
+      uniqueColors.add('currentColor');
+    }
+
+    // Return the size of the Set (number of unique colors)
+    return uniqueColors.size;
   }
 
   onCancelClicked() {

b9c40ee79a1e757aedd03c559219335cf2d94d36
Chris Wood <hello@chris-wood.design>
2024-05-24 01:23:19 +0200

    Enable asynchronous application thread

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import 'gi://Gtk?version=4.0';
 import { Application } from './Application.js';
 
 export function main(argv) {
-	return new Application({ 'application-id': pkg.name }).run(argv);
+	return new Application({ 'application-id': pkg.name }).runAsync(argv);
 }
 

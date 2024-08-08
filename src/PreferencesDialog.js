c053a90cab48aa34c48ed3a3fcc3414c307cf458
Chris Wood <hello@chris-wood.design>
2024-08-08 01:34:32 +0100

    Create a new setting called "Preferred copy method" and bind its value to the ComboRow in the PreferencesDialog

--- src/PreferencesDialog.js
+++ src/PreferencesDialog.js
@@ -8,8 +8,29 @@ import GLib from 'gi://GLib';
 export const PreferencesDialog = GObject.registerClass({
   GTypeName: 'IcoPreferencesDialog',
   Template: 'resource:///design/chris_wood/IconBear/ui/PreferencesDialog.ui',
+  InternalChildren: ['preferred_copy_method_input'],
+  Properties: {
+   preferredCopyMethod: GObject.ParamSpec.int(
+      'preferredCopyMethod',
+      'Preferred Copy Method',
+      'The user-defined setting for the default copy method',
+      GObject.ParamFlags.READWRITE,
+      0, 1,
+      0
+    ),
+  }
 }, class extends Gtk.Widget {
   constructor(params){
     super(params);
+    this.bindToSettings();
+    this.bindProperties();
+  }
+
+  bindToSettings() {
+    settings.bind('preferred-copy-method', this, 'preferredCopyMethod', Gio.SettingsBindFlags.DEFAULT);
+  }
+
+  bindProperties() {
+    this.bind_property('preferredCopyMethod', this._preferred_copy_method_input, 'selected', GObject.BindingFlags.BIDIRECTIONAL);
   }
 });
